509ee9ab02ed9e08b93eee85cf0c8057fb7367b4
Bryan Slatner <bryan@slatner.com>
2020-05-27 17:20:55 -0400

    Add module for evaluation

---
+++ DiceRoller/Eval.fs
@@ -0,0 +1,38 @@
+﻿module Eval
+
+open Syntax
+
+let private rnd = System.Random()
+
+let private rollDie sides =
+    rnd.Next(sides) + 1
+
+let private roll quantity sides =
+    seq {
+        for i in 1..quantity -> (rollDie sides)
+    } |> Seq.sum
+
+//let rec updateFactor factor =
+//    match factor with
+//    | DiceRoll diceRoll -> Factor.Value (roll diceRoll.Quantity diceRoll.Sides)
+//    | Expression term -> updateTerm term
+//    | AddOp (l,op,r) ->
+//        let newl = updateFactor l
+//        let newr = updateFactor r
+//        Factor.AddOp newl,op,newr
+//    | _ -> factor
+
+//and updateTerm term =
+//    match term with
+//    | Term.Factor factor -> Term.Factor (updateFactor factor)
+//    | MulOp (l,op,r) ->
+//let newl = updateFactor l
+//let newr = updateFactor r
+//Factor.AddOp newl,op,newr
+
+
+//let rec rollDice expression
+//    match expression with
+//    | Term t ->
+//        match t with
+//        |
