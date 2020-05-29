319ac40e3869ba98c9373db5a72ab544d2fa7ec4
Bryan Slatner <bryan@slatner.com>
2020-05-29 16:58:29 -0400

    Actually use command line arguments

--- DiceRoller/Program.fs
+++ DiceRoller/Program.fs
@@ -4,13 +4,14 @@ open Eval
 
 [<EntryPoint>]
 let main argv =
-    //let parsed,rest = parseDiceExpression "1+(2-3*4+5)*6"
-    //let parsed,rest = parseDiceExpression "2-3*4+5"
-    let parsed,rest = parseDiceExpression "2-3*4+5"
-    //if not (String.IsNullOrWhiteSpace(rest)) then
-    //    printfn "Syntax error at: %s" rest
-    //else
-    //    let result = eval parsed
-    //    printfn "Parsed: %A" parsed
-    //    printfn "Result: %i" result
+    let expression = String.Join(" ",argv)
+    let parsed,rest = parseDiceExpression expression
+    if not (String.IsNullOrWhiteSpace(rest)) then
+        printfn "Syntax error at: %s" rest
+    else
+        let result = eval parsed
+        #if DEBUG
+        printfn "Parsed: %A" parsed
+        #endif
+        printfn "Result: %i" result
     0 // return an integer exit code
