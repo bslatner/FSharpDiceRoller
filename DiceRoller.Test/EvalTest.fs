7d4865b8a0a7b29ca500e15e883f89093739e0c1
Bryan Slatner <bryan@slatner.com>
2020-05-29 11:28:27 -0400

    Multiplication tests

--- DiceRoller.Test/EvalTest.fs
+++ DiceRoller.Test/EvalTest.fs
@@ -3,7 +3,8 @@
 open Expecto
 open Syntax
 
-let opify = List.map (fun (op,v) -> op,(valuet v))
+let addopify = List.map (fun (op,v) -> op,(valuet v))
+let mulopify = List.map (fun (op,v) -> op,(valuef v))
 
 [<Tests>]
 let tests = 
@@ -25,13 +26,27 @@ let tests =
                     Expect.evaluatesTo (sube l r) (l - r)
 
         testCase "Consecutive addition" <| fun _ ->
-            Expect.evaluatesTo (AddOp (valuet 1, (opify [Plus,2; Plus,3]))) 6
-            Expect.evaluatesTo (AddOp (valuet 1, (opify [Plus,2; Plus,3; Plus,4]))) 10
-            Expect.evaluatesTo (AddOp (valuet 1, (opify [Plus,2; Plus,3; Plus,4; Plus,5]))) 15
-            Expect.evaluatesTo (AddOp (valuet 1, (opify [Plus,2; Plus,3; Plus,4; Minus,5]))) 5
-            Expect.evaluatesTo (AddOp (valuet 1, (opify [Plus,2; Minus,3]))) 0
-            Expect.evaluatesTo (AddOp (valuet 1, (opify [Plus,2; Minus,3; Plus,4]))) 4
-            Expect.evaluatesTo (AddOp (valuet 1, (opify [Plus,2; Plus,3; Minus,4; Plus,5]))) 7
+            Expect.evaluatesTo (AddOp (valuet 1, (addopify [Plus,2; Plus,3]))) 6
+            Expect.evaluatesTo (AddOp (valuet 1, (addopify [Plus,2; Plus,3; Plus,4]))) 10
+            Expect.evaluatesTo (AddOp (valuet 1, (addopify [Plus,2; Plus,3; Plus,4; Plus,5]))) 15
+            Expect.evaluatesTo (AddOp (valuet 1, (addopify [Plus,2; Plus,3; Plus,4; Minus,5]))) 5
+            Expect.evaluatesTo (AddOp (valuet 1, (addopify [Plus,2; Minus,3]))) 0
+            Expect.evaluatesTo (AddOp (valuet 1, (addopify [Plus,2; Minus,3; Plus,4]))) 4
+            Expect.evaluatesTo (AddOp (valuet 1, (addopify [Plus,2; Plus,3; Minus,4; Plus,5]))) 7
+
+        testCase "Consecutive multiplication" <| fun _ ->
+            Expect.evaluatesTo (Expression.Term (MulOp (valuef 1,(mulopify [Multiply,2;Multiply,3])))) 6
+            Expect.evaluatesTo (Expression.Term (MulOp (valuef 2,(mulopify [Multiply,3;Multiply,4;Multiply,5])))) 120
+
+        testCase "AddOps with DieRolls" <| fun _ ->
+            Expect.evaluatesTo (AddOp (rollt 1 20, (addopify [Plus,1]))) 21
+            Expect.evaluatesTo (AddOp (rollt 1 20, (addopify [Minus,1]))) 19
+            Expect.evaluatesTo (AddOp (valuet 1, [Plus,rollt 1 20])) 21
+            Expect.evaluatesTo (AddOp (valuet 21, [Minus,rollt 1 20])) 1
+
+        testCase "MulOps with DieRolls" <| fun _ ->
+            Expect.evaluatesTo (Expression.Term (MulOp (rollf 1 20,(mulopify [Multiply,2])))) 40
+            Expect.evaluatesTo (Expression.Term (MulOp (rollf 2 6,(mulopify [Multiply,2])))) 24
 
         testCase "Parentheticals" <| fun _ ->
             Expect.evaluatesTo (AddOp (valuet 10, ([Minus,addt 5 5]))) 0
--- DiceRoller.Test/Helpers.fs
+++ DiceRoller.Test/Helpers.fs
@@ -7,7 +7,8 @@ let valuef x = Value x
 let valuet x = Term.Factor (valuef x)
 let valuee x = Expression.Term (valuet x)
 
-let termify x = Term.Factor (Factor.Expression x)
+let termifye x = Term.Factor (Factor.Expression x)
+let expressifyf x = Expression.Term (Term.Factor x)
 
 let adde x y = AddOp(valuet x,[Plus,valuet y])
 let sube x y = AddOp(valuet x,[Minus,valuet y])
@@ -19,3 +20,6 @@ let mult x y = MulOp(valuef x,[Multiply,valuef y])
 let mule x y = Expression.Term (mult x y)
 let mulf x y = Factor.Expression (mule x y)
 
+let rollf q s = DiceRoll (q,s)
+let rollt q s = Term.Factor (rollf q s)
+let rolle q s = Expression.Term (rollt q s)
