71656b0bed1440ffea19ba75c95f7b1e7f4e8d45
Bryan Slatner <bryan@slatner.com>
2020-05-29 11:03:03 -0400

    Use constant value for tests with die rolls

--- DiceRoller.Test/Program.fs
+++ DiceRoller.Test/Program.fs
@@ -2,4 +2,5 @@
 
 [<EntryPoint>]
 let main argv =
+    Eval.rollDie <- (fun sides -> sides) // when rolling a die in a test, just return the number of sides
     Tests.runTestsInAssembly defaultConfig argv
